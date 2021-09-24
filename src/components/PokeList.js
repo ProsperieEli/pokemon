dda0d38b8489b24fc970c87c0826ee97c57076e1
ProsperieEli <ElijahProsperie@gmail.com>
2021-09-23 21:22:53 -0400

    Near done

---
+++ src/components/PokeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PokeItem from './PokeItem'
+
+export default class PokeList extends Component {
+    render() {
+
+        return (
+            <ul>
+                {this.props.pokeArray.map(poke => (
+                    <PokeItem key = {poke.pokemon}
+                    image={poke.url_image}
+                    name={poke.pokemon}
+                    type={poke.type_1}
+                />))}
+               
+            </ul>
+        )
+    }
+}
